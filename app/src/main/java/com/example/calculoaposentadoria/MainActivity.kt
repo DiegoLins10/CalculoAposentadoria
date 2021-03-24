a8a4df9c8d116a1c069f27de99fe0b4bb482b4f1
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-03-24 11:17:53 -0300

    aposentadoria 24/03

---
+++ app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.calculoaposentadoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spn_Sexo = findViewById<Spinner>(R.id.spn_Sexo)
+        val txt_Idade = findViewById<TextView>(R.id.txt_Idade)
+        val btn_Calcular = findViewById<Button>(R.id.btn_Calcular)
+        val txt_Resultado = findViewById<TextView>(R.id.txt_Resultado)
+
+        spn_Sexo.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
+            listOf("Masculino","Feminino"))
+
+        //btn_Calcular_onclick
+        btn_Calcular.setOnClickListener{
+            val sexo = spn_Sexo.selectedItem as String
+            val idade = txt_Idade.text.toString().toInt()
+            var resultado = 0
+            if(sexo == "Masculino"){
+                resultado = 70 - idade
+            } else{
+                resultado = 65 - idade
+            }
+            txt_Resultado.text = "Faltam $resultado anos para você se aposentar"
+
+        }
+    }
+}
