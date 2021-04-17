e48186c121fddc9f5b2396400b872b3098fade21
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-04-17 19:04:51 -0300

    att exercise professor

--- app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
+++ app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
@@ -16,20 +16,28 @@ class MainActivity : AppCompatActivity() {
         val txt_Resultado = findViewById<TextView>(R.id.txt_Resultado)
 
         spn_Sexo.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
-            listOf("Masculino","Feminino"))
+                listOf("Masculino", "Feminino"))
+
 
         //btn_Calcular_onclick
-        btn_Calcular.setOnClickListener{
-            val sexo = spn_Sexo.selectedItem as String
-            val idade = txt_Idade.text.toString().toInt()
-            var resultado = 0
-            if(sexo == "Masculino"){
-                resultado = 70 - idade
-            } else{
-                resultado = 65 - idade
-            }
-            txt_Resultado.text = "Faltam $resultado anos para você se aposentar"
+        btn_Calcular.setOnClickListener {
+            if (txt_Idade.text.isEmpty()) {
+                //validação de dados
+                txt_Idade.error = "Digite a idade: "
+                Toast.makeText(this, "CAMPO VAZIO",Toast.LENGTH_LONG).show();
+            } else {
+                val sexo = spn_Sexo.selectedItem as String
+                val idade = txt_Idade.text.toString().toInt()
+                var resultado = 0
+                if (sexo == "Masculino") {
+                    resultado = 65 - idade
+                } else {
+                    resultado = 60 - idade
+                }
 
+                txt_Resultado.text = "Faltam $resultado anos para você se aposentar"
+
+            }
         }
     }
 }
